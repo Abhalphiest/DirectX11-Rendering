67d81712f2cd017b8b45ec6342f24dc0b63ee114
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-10-28 09:00:12 -0400

    created scene tool

---
+++ SceneTool/SceneTool/Main.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SceneTool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scenewindow = new System.Windows.Forms.Panel();
+            this.mainpanel = new System.Windows.Forms.Panel();
+            this.addmesh = new System.Windows.Forms.Button();
+            this.addtexture = new System.Windows.Forms.Button();
+            this.creatematerial = new System.Windows.Forms.Button();
+            this.createobject = new System.Windows.Forms.Button();
+            this.objectlist = new System.Windows.Forms.ListBox();
+            this.createlight = new System.Windows.Forms.Button();
+            this.addshader = new System.Windows.Forms.Button();
+            this.addlabel = new System.Windows.Forms.Label();
+            this.mainpanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // scenewindow
+            // 
+            this.scenewindow.Location = new System.Drawing.Point(12, 12);
+            this.scenewindow.Name = "scenewindow";
+            this.scenewindow.Size = new System.Drawing.Size(400, 336);
+            this.scenewindow.TabIndex = 0;
+            // 
+            // mainpanel
+            // 
+            this.mainpanel.Controls.Add(this.addlabel);
+            this.mainpanel.Controls.Add(this.addshader);
+            this.mainpanel.Controls.Add(this.createlight);
+            this.mainpanel.Controls.Add(this.objectlist);
+            this.mainpanel.Controls.Add(this.createobject);
+            this.mainpanel.Controls.Add(this.creatematerial);
+            this.mainpanel.Controls.Add(this.addtexture);
+            this.mainpanel.Controls.Add(this.addmesh);
+            this.mainpanel.Location = new System.Drawing.Point(433, 12);
+            this.mainpanel.Name = "mainpanel";
+            this.mainpanel.Size = new System.Drawing.Size(196, 336);
+            this.mainpanel.TabIndex = 1;
+            // 
+            // addmesh
+            // 
+            this.addmesh.Location = new System.Drawing.Point(44, 8);
+            this.addmesh.Name = "addmesh";
+            this.addmesh.Size = new System.Drawing.Size(110, 23);
+            this.addmesh.TabIndex = 0;
+            this.addmesh.Text = "Add Mesh";
+            this.addmesh.UseVisualStyleBackColor = true;
+            // 
+            // addtexture
+            // 
+            this.addtexture.Location = new System.Drawing.Point(44, 66);
+            this.addtexture.Name = "addtexture";
+            this.addtexture.Size = new System.Drawing.Size(110, 23);
+            this.addtexture.TabIndex = 1;
+            this.addtexture.Text = "Add Texture";
+            this.addtexture.UseVisualStyleBackColor = true;
+            // 
+            // creatematerial
+            // 
+            this.creatematerial.Location = new System.Drawing.Point(44, 95);
+            this.creatematerial.Name = "creatematerial";
+            this.creatematerial.Size = new System.Drawing.Size(110, 22);
+            this.creatematerial.TabIndex = 2;
+            this.creatematerial.Text = "Create Material";
+            this.creatematerial.UseVisualStyleBackColor = true;
+            // 
+            // createobject
+            // 
+            this.createobject.Location = new System.Drawing.Point(44, 123);
+            this.createobject.Name = "createobject";
+            this.createobject.Size = new System.Drawing.Size(110, 23);
+            this.createobject.TabIndex = 3;
+            this.createobject.Text = "Create Object";
+            this.createobject.UseVisualStyleBackColor = true;
+            // 
+            // objectlist
+            // 
+            this.objectlist.FormattingEnabled = true;
+            this.objectlist.Location = new System.Drawing.Point(32, 211);
+            this.objectlist.Name = "objectlist";
+            this.objectlist.Size = new System.Drawing.Size(132, 108);
+            this.objectlist.TabIndex = 4;
+            // 
+            // createlight
+            // 
+            this.createlight.Location = new System.Drawing.Point(44, 152);
+            this.createlight.Name = "createlight";
+            this.createlight.Size = new System.Drawing.Size(110, 23);
+            this.createlight.TabIndex = 5;
+            this.createlight.Text = "Create Light";
+            this.createlight.UseVisualStyleBackColor = true;
+            // 
+            // addshader
+            // 
+            this.addshader.Location = new System.Drawing.Point(44, 37);
+            this.addshader.Name = "addshader";
+            this.addshader.Size = new System.Drawing.Size(110, 23);
+            this.addshader.TabIndex = 6;
+            this.addshader.Text = "Add Shader";
+            this.addshader.UseVisualStyleBackColor = true;
+            // 
+            // addlabel
+            // 
+            this.addlabel.AutoSize = true;
+            this.addlabel.Location = new System.Drawing.Point(32, 192);
+            this.addlabel.Name = "addlabel";
+            this.addlabel.Size = new System.Drawing.Size(72, 13);
+            this.addlabel.TabIndex = 7;
+            this.addlabel.Text = "Add to Scene";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 360);
+            this.Controls.Add(this.mainpanel);
+            this.Controls.Add(this.scenewindow);
+            this.Name = "Main";
+            this.Text = "Scene Tool";
+            this.mainpanel.ResumeLayout(false);
+            this.mainpanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel scenewindow;
+        private System.Windows.Forms.Panel mainpanel;
+        private System.Windows.Forms.Button addshader;
+        private System.Windows.Forms.Button createlight;
+        private System.Windows.Forms.ListBox objectlist;
+        private System.Windows.Forms.Button createobject;
+        private System.Windows.Forms.Button creatematerial;
+        private System.Windows.Forms.Button addtexture;
+        private System.Windows.Forms.Button addmesh;
+        private System.Windows.Forms.Label addlabel;
+    }
+}
---
+++ SceneTool/SceneTool/Start.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SceneTool
+{
+    partial class startscreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newscene = new System.Windows.Forms.Button();
+            this.editscene = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // newscene
+            // 
+            this.newscene.Location = new System.Drawing.Point(178, 82);
+            this.newscene.Name = "newscene";
+            this.newscene.Size = new System.Drawing.Size(169, 23);
+            this.newscene.TabIndex = 0;
+            this.newscene.Text = "New Scene";
+            this.newscene.UseVisualStyleBackColor = true;
+            // 
+            // editscene
+            // 
+            this.editscene.Location = new System.Drawing.Point(178, 143);
+            this.editscene.Name = "editscene";
+            this.editscene.Size = new System.Drawing.Size(169, 23);
+            this.editscene.TabIndex = 1;
+            this.editscene.Text = "Edit Existing Scene";
+            this.editscene.UseVisualStyleBackColor = true;
+            // 
+            // startscreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 272);
+            this.Controls.Add(this.editscene);
+            this.Controls.Add(this.newscene);
+            this.Name = "startscreen";
+            this.Text = "Scene Tool";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newscene;
+        private System.Windows.Forms.Button editscene;
+    }
+}
+
